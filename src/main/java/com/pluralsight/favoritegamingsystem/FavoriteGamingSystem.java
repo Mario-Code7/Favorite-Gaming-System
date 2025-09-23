ff64f6c5e526c8e337d13fcbd6d67df4ca3081a2
Mario-Code7 <marioroa71@yahoo.com>
2025-09-23 15:42:35 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/favoritegamingsystem/FavoriteGamingSystem.java
@@ -0,0 +1,7 @@
+package com.pluralsight.favoritegamingsystem;
+
+public class FavoriteGamingSystem {
+    public static void main(String[] args) {
+        System.out.println("PS5, Xbox, Steam Deck, PC, Switch");
+    }
+}
