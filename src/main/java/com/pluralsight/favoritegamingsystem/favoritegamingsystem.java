8b8558a278b82d18a6f2c224fed9831c6ef3fc12
Mario-Code7 <marioroa71@yahoo.com>
2025-09-24 09:20:14 -0500

    updated artifactid

--- src/main/java/com/pluralsight/favoritegamingsystem/favoritegamingsystem.java
+++ src/main/java/com/pluralsight/favoritegamingsystem/favoritegamingsystem.java
@@ -1,6 +1,6 @@
 package com.pluralsight.favoritegamingsystem;
 
-public class FavoriteGamingSystem {
+public class favoritegamingsystem {
     public static void main(String[] args) {
         System.out.println("PS5, Xbox, Steam Deck, PC, Switch");
     }
